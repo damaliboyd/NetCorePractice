d6d2a18363a94ce91a1794e6beb9c69bdb3d9cae
Damali Boyd <damaliboyd@ymail.com>
2021-02-28 21:53:00 -0500

    Added company and employee entities created repository for them

---
+++ src/Entities/Repository/RepositoryContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Repository
+{
+    class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
